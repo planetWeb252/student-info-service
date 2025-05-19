f258e7b33dc6bc543fbe9db42a4b1977203b919d
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-05-19 15:09:39 +0200

    repo with dependencies

---
+++ src/main/java/com/student/info_service/StudentInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.student.info_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/student/info_service/StudentInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.info_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
